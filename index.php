60b661e52369cf7a3807bbcec95d46328a93762f
Stuart Bayford <stu@stuartbayford.co.uk>
2018-01-04 22:27:53 +0000

    Setup

---
+++ index.php
@@ -0,0 +1,58 @@
+<html lang="en">
+	<!-- Head -->
+	<head>
+		<!--Metadata-->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="author" content="Stu Bayford">
+		<meta name="description" content="The Yorkshire Corker is a charity bike ride set in the beautiful scenery of Yorkshire">
+		<meta name="keywords" content="Bike, Ride, Cycle, Charity, Yorkshire">
+		
+		<!-- Title -->
+		<title>The Yorkshire Corker</title>
+		
+		<!-- Stylesheets -->
+		<link rel="stylesheet" href="styles/css/style.css">
+		
+		<!-- Font Awesome -->
+		<script src="https://use.fontawesome.com/b722fb01b9.js"></script>
+	</head>
+	
+	<!-- Body -->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand full-brand" href="#">
+        	<img src="assets/graphics/yorkshire-corker-logo.png" width="40" height="40" alt="Yorkshire Corker Logo">
+          The Yorkshire Corker
+        </a>
+<!--         <a class="navbar-brand abbr-brand" href="#">
+          <img src="assets/graphics/yorkshire-corker-logo.png" width="40" height="40" alt="Yorkshire Corker Logo">
+        </a> -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbar">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#home">Home<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#routes">Routes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#charities">Charities</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-md-0">
+            <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+          </form>
+        </div>
+      </div>
+    </nav>
